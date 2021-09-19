15e9e1571f97b8aaee39896448f7a0f396fa6a2d
Pollock Rockstaedter <robinkuprella@gmail.com>
2021-09-19 17:57:02 +0200

    page content

--- src/lib/PageContact.svelte
+++ src/lib/PageContact.svelte
@@ -1,16 +1,11 @@
 <script>
   export let domain;
   import AppPageWrapper from "$lib/AppPageWrapper.svelte";
+  import PagePhoneButton from "$lib/PagePhoneButton.svelte";
 </script>
 
 <AppPageWrapper>
   <div>
-    <h2 class="font-bold text-2xl mb-6">Kontakt</h2>
-    <ul>
-      <li>{domain.brand.service} {domain.brand.name}</li>
-      <li class="mt-4">Telefon: {domain.brand.imprint.address.phone}</li>
-      <li class="mb-12">Email: {domain.brand.imprint.email}</li>
-    </ul>
     <h3 class="font-bold text-2xl mb-6">Wir sind für Sie da</h3>
     <p>
       Wenn Sie einen Schlüsseldienst brauchen, rufen Sie uns an. Wenn Sie Fragen
@@ -18,5 +13,15 @@
       Sie uns eine Email. Wir beantworten Emails in der Regel innerhalb von
       einem Werktag.
     </p>
+    <div class="py-12 flex justify-start">
+      <PagePhoneButton phone={domain.address.phone} />
+    </div>
+    <ul>
+      <li>
+        <strong>{domain.brand.service} {domain.brand.name}</strong>
+      </li>
+      <li>Email: {domain.brand.imprint.email}</li>
+    </ul>
   </div>
+
 </AppPageWrapper>
