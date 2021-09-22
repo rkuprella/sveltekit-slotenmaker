eee2f436b8001c2ed0317cf2873eecbf86d959e9
Pollock Rockstaedter <robinkuprella@gmail.com>
2021-09-22 14:37:32 +0200

    static tests

--- svelte.config.js
+++ svelte.config.js
@@ -5,9 +5,7 @@ const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
-    adapter: adapter({
-      fallback: "200.html",
-    }),
+    adapter: adapter(),
   },
 
   preprocess: [
